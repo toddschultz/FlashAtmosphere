b2c0d72c2e67ad5bd6b7a8f78970752a01b2c385
Todd Schultz <toddschultz@gmail.com>
2022-11-16 19:01:17 -0800

    Update ReadSensors.ino

--- ENVSimple/ReadSensors/ReadSensors.ino
+++ ENVSimple/ReadSensors/ReadSensors.ino
@@ -56,7 +56,7 @@ DHT dht(DHTPIN, DHTTYPE);
 #include <Arduino_MKRENV.h>
 
 // ENV temperature offset correction
-const float deltatempC = 11*5/9;
+const float deltatempC = 0; //11*5/9;
 
 // Wifi stuff
 #include <WiFiNINA.h>
